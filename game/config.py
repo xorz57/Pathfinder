8e2682c2094678af217ccc0c696fbf140e186361
George Fotopoulos <xorz57@gmail.com>
2023-11-10 16:38:53 +0200

    Update config.py

--- game/config.py
+++ game/config.py
@@ -1,10 +1,10 @@
 # Tile settings
-TILE_W = 10
-TILE_H = 10
+TILE_W = 5
+TILE_H = 5
 
 # Board settings
-BOARD_ROWS = 80
-BOARD_COLS = 80
+BOARD_ROWS = 160
+BOARD_COLS = 160
 
 # Window settings
 WINDOW_W = BOARD_COLS * TILE_W
