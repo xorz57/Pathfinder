8c50a29a475031d3dd1221d48771789abc0b578d
George Fotopoulos <xorz57@gmail.com>
2023-11-10 02:33:49 +0200

    Fix A* algorithm

--- game/algorithm.py
+++ game/algorithm.py
@@ -5,7 +5,7 @@ import time
 
 
 def _heuristic(a, b):
-    return math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
+    return math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2)
 
 
 def _reconstruct_path(current):
@@ -17,9 +17,9 @@ def _reconstruct_path(current):
 
 
 def astar(start, finish, delay=0.025):
-    frontier = []
     start.g = 0
-    start.f = start.g + _heuristic(start, finish)
+    start.f = 0 + _heuristic(start, finish)
+    frontier = []
     heapq.heappush(frontier, (start.f, start))
     while frontier:
         time.sleep(delay)
@@ -33,12 +33,12 @@ def astar(start, finish, delay=0.025):
         for neighbor in current.neighbors:
             if neighbor.visited or neighbor.state == "wall":
                 continue
-            g = current.g + _heuristic(current, neighbor)
+            g = current.g + 1
             if g < neighbor.g:
                 if neighbor != start and neighbor != finish:
                     neighbor.state = "frontier"
                 neighbor.g = g
-                neighbor.f = neighbor.g + _heuristic(neighbor, finish)
+                neighbor.f = g + _heuristic(neighbor, finish)
                 neighbor.visited = True
                 neighbor.parent = current
                 heapq.heappush(frontier, (neighbor.f, neighbor))
